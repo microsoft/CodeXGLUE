8085e24acaf531ad7adc016ce04bf3b398ba669b
Jun-jie-Huang <JunjayHuang@outlook.com>
2020-09-02 14:19:42 +0800

    initial commit of webpage

---
+++ webpage_files/scrolling-nav.css
@@ -0,0 +1,12 @@
+/*!
+ * Start Bootstrap - Scrolling Nav (https://startbootstrap.com/template-overviews/scrolling-nav)
+ * Copyright 2013-2019 Start Bootstrap
+ * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-scrolling-nav/blob/master/LICENSE)
+ */
+header {
+  padding: 156px 0 100px;
+}
+
+section {
+  padding: 150px 0;
+}

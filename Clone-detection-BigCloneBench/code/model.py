a9760282409e6b93e44a6bddb2a38481d37cc6a8
guody5 <824854534@qq.com>
2020-09-02 15:20:46 +0800

    Update model.py

--- Clone-detection-BigCloneBench/code/model.py
+++ Clone-detection-BigCloneBench/code/model.py
@@ -1,5 +1,5 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
+# Copyright (c) Microsoft Corporation. 
+# Licensed under the MIT license.
 import torch
 import torch.nn as nn
 import torch

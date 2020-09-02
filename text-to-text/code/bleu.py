d9b26612b87ff04f035d5d4b6673a7f65477f763
Long Zhou <wszlong@gmail.com>
2020-09-02 15:18:11 +0800

    Update bleu.py

--- text-to-text/code/bleu.py
+++ text-to-text/code/bleu.py
@@ -1,5 +1,5 @@
-## Copyright (c) Microsoft Corporation.
-## Licensed under the MIT License.
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
 
 import collections
 import math

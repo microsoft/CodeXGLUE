d8d268e9b369f2f084b8dac1ac630409214270a8
Imagist-Shuo <shuoren@buaa.edu.cn>
2020-09-29 14:28:38 +0800

    reorganize folders

--- Code-Code/code-to-code-trans/code/model3/parser/tree-sitter-ruby/test/corpus/single-cr-as-whitespace.rb
+++
@@ -1,9 +0,0 @@
-==================================
-Single CR characters as whitespace
-==================================
-
-puts"hi"
-
----
-
-(program (method_call (identifier) (argument_list (string))))
